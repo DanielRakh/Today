d4e491216468c3b2b611c0c2a5f5c3018e19686c
Daniel Rakhamimov <danielrak@me.com>
2014-10-21 17:14:16 -0400

    Spec for MainViewController

---
+++ TodayTests/MainViewControllerSpec.swift
@@ -0,0 +1,21 @@
+import Quick
+import Nimble
+import UIKit
+
+class MainViewControllerSpec: QuickSpec {
+    override func spec() {
+        var viewController:MainViewController!
+        
+        beforeEach {
+            let storyboard = UIStoryboard(name: "Main", bundle: NSBundle(forClass:self.dynamicType))
+            let navController:UINavigationController = storyboard.instantiateInitialViewController() as UINavigationController
+            viewController = navController.topViewController as? MainViewController
+        }
+        
+        describe("Initialization") {
+            it("Is not nil") {
+                expect(viewController).toNot(beNil())
+            }
+        }
+    }
+}
