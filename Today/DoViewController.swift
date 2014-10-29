5869a7e7c3d14e305bb20e3b2dc6e584a7454bac
Daniel Rakhamimov <danielrak@me.com>
2014-10-29 13:14:49 -0400

    DoViewController TableView

---
+++ Today/DoViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DoViewController.swift
+//  Today
+//
+//  Created by Daniel on 10/29/14.
+//  Copyright (c) 2014 Daniel Rakhamimov. All rights reserved.
+//
+
+import UIKit
+
+class DoViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension DoViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("DoCell") as UITableViewCell
+        cell.textLabel.text = "\(indexPath.row)"
+        return cell
+    }
+}
+
+
