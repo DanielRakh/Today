70b35603cc0483e9b94c6cf42911244e9921f703
Daniel Rakhamimov <danielrak@me.com>
2014-11-05 13:55:53 -0500

    AddEntryView

---
+++ Today/AddEntryView.swift
@@ -0,0 +1,110 @@
+//
+//  AddEntryView.swift
+//  Today
+//
+//  Created by Daniel on 11/4/14.
+//  Copyright (c) 2014 Daniel Rakhamimov. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+@IBDesignable class AddEntryView: UIButton {
+    
+    var additionGlyph:CALayer!
+    
+    @IBInspectable var glyphSizeAsPercentageOfSuperView:CGFloat = 0
+    @IBInspectable var glyphColor:UIColor = UIColor.todayWhite
+    @IBInspectable var cornerRadius:CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+//        setup()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+//        setup()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        //        setup()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setup()
+    }
+
+    func setup() {
+        additionGlyph = createAdditionGlyph()
+        layer.addSublayer(additionGlyph)
+    }
+    
+    
+    //MARK: Addition Glyph Drawing
+    
+    enum LineType {
+        case Horizontal
+        case Vertical
+    }
+    
+    func createAdditionGlyph() -> CALayer {
+        let lineLength:CGFloat = self.bounds.size.width * glyphSizeAsPercentageOfSuperView
+        
+        let horizontalLine = lineShapeForPath(bezierPathForLine(
+            lineType: .Horizontal, length: lineLength))
+        let verticalLine = lineShapeForPath(bezierPathForLine(
+            lineType: .Vertical, length: lineLength))
+        let additionGlyphLayer = CALayer()
+        
+        additionGlyphLayer.bounds = CGRectMake(0, 0, CGRectGetWidth(horizontalLine.bounds), CGRectGetHeight(horizontalLine.bounds))
+        additionGlyphLayer.anchorPoint = CGPointMake(0.5, 0.5)
+        additionGlyphLayer.position = CGPointMake(CGRectGetWidth(self.bounds)/2, CGRectGetHeight(self.bounds)/2)
+        
+        horizontalLine.anchorPoint = CGPointMake(0.5, 0.5)
+        horizontalLine.position = CGPointMake(CGRectGetWidth(additionGlyphLayer.bounds)/2, CGRectGetHeight(additionGlyphLayer.bounds)/2)
+       
+        verticalLine.anchorPoint = CGPointMake(0.5, 0.5)
+        verticalLine.position = CGPointMake(CGRectGetWidth(additionGlyphLayer.bounds)/2, CGRectGetHeight(additionGlyphLayer.bounds)/2)
+        
+        additionGlyphLayer.addSublayer(horizontalLine)
+        additionGlyphLayer.addSublayer(verticalLine)
+        
+        return additionGlyphLayer
+    }
+    
+    func lineShapeForPath(path:UIBezierPath) -> CAShapeLayer {
+        let shape = CAShapeLayer()
+        shape.path = path.CGPath
+        shape.bounds = CGRectMake(0, 0, path.bounds.size.width, path.bounds.size.height)
+        shape.lineWidth = 2.0
+        shape.strokeColor = glyphColor.CGColor
+        return shape
+    }
+    
+    func bezierPathForLine(#lineType:LineType, length:CGFloat) -> UIBezierPath {
+        let lineBezier = UIBezierPath()
+        var endPoint:CGPoint!
+        switch lineType {
+            case .Horizontal:
+                endPoint = CGPointMake(length, 0)
+            case .Vertical:
+                endPoint = CGPointMake(0, length)
+            default:
+                endPoint = CGPointZero
+        }
+        lineBezier.moveToPoint(CGPointZero)
+        lineBezier.addLineToPoint(endPoint)
+        return lineBezier
+    }
+
+}
+
+
+
