fde61b649b135ef8f26d2752201d8cb285b4001c
Daniel Rakhamimov <danielrak@me.com>
2014-11-08 02:08:35 -0500

    DRTabBar is visually a go!

--- Today/DRTabBar.swift
+++ Today/DRTabBar.swift
@@ -13,6 +13,11 @@ protocol DRTabBarDelegate {
     func dontButtonDidTouch(sender:AnyObject)
 }
 
+enum ButtonMode {
+    case Dont
+    case Do
+}
+
 
 class DRTabBar: UIView {
     
@@ -21,16 +26,107 @@ class DRTabBar: UIView {
     @IBOutlet weak var dontButton:UIButton!
     @IBOutlet weak var doButton:UIButton!
     
+    @IBOutlet weak var dontLabel:UILabel!
+    @IBOutlet weak var doLabel:UILabel!
+    
+    @IBOutlet weak var underlineView:UIView!
+    
+    
+    @IBOutlet var centerXAlignUnderlineToDoLabel:NSLayoutConstraint!
+    @IBOutlet var equalWidthUnderlineToDoLabel:NSLayoutConstraint!
+    var centerXAlignUnderlineToDontLabel:NSLayoutConstraint?
+    var equalWidthUnderlineToDontLabel:NSLayoutConstraint?
+    
+    var currentButtonSelected:ButtonMode? {
+        didSet {
+            if currentButtonSelected != nil {
+                dontLabel.textColor = currentButtonSelected == .Do ? UIColor.darkGrayColor() : UIColor.todayWhite
+                doLabel.textColor = currentButtonSelected == .Do ? UIColor.todayWhite : UIColor.darkGrayColor()
+            }
+        }
+    }
+    
+    override func updateConstraints() {
+        if (centerXAlignUnderlineToDontLabel == nil && equalWidthUnderlineToDontLabel == nil) {
+            setupDontLabelConstraints()
+        }
+            super.updateConstraints()
+        }
+    
+    required init(coder aDecoder: NSCoder) {
+        currentButtonSelected = .Do
+        super.init(coder: aDecoder)
+    }
+ 
+    //MARK: Helpers
+    
+    func setupDontLabelConstraints() {
+        centerXAlignUnderlineToDontLabel = NSLayoutConstraint(
+            item: underlineView,
+            attribute: .CenterX,
+            relatedBy: .Equal,
+            toItem: dontLabel,
+            attribute: .CenterX,
+            multiplier: 1,
+            constant: 0)
+        
+        equalWidthUnderlineToDontLabel = NSLayoutConstraint(
+            item: underlineView,
+            attribute: .Width,
+            relatedBy: .Equal,
+            toItem: dontLabel,
+            attribute: .Width,
+            multiplier: 1,
+            constant: 0)
+    }
+    
+    
+    func performUnderlineAnimationForMode(mode:ButtonMode) {
+        
+        adjustContraintsForMode(mode)
+    
+        UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping:0.7, initialSpringVelocity:1.0, options: .CurveEaseInOut, animations: {
+            UIView.performWithoutAnimation({ () -> Void in
+                self.currentButtonSelected = mode
+            })
+                self.layoutIfNeeded()
+            }, completion: { finished in
+                //
+        })
+        
+    }
+    
+    func adjustContraintsForMode(mode:ButtonMode) {
+        switch mode {
+        case .Do:
+            removeConstraints([centerXAlignUnderlineToDontLabel!, equalWidthUnderlineToDontLabel!])
+            setNeedsUpdateConstraints()
+            addConstraints([centerXAlignUnderlineToDoLabel,equalWidthUnderlineToDoLabel])
+        case .Dont:
+            removeConstraints([centerXAlignUnderlineToDoLabel,equalWidthUnderlineToDoLabel])
+            setNeedsUpdateConstraints()
+            addConstraints([centerXAlignUnderlineToDontLabel!, equalWidthUnderlineToDontLabel!])
+           
+        default:
+            println("There was an error adjusting constraints for mode")
+        }
+    }
     
     //MARK: IBActions
     
     @IBAction func doButtonPressed(sender:AnyObject) {
-        println("Do Button Pressed")
-        delegate?.doButtonDidTouch(sender)
+        if currentButtonSelected == .Dont {
+            delegate?.doButtonDidTouch(sender)
+            performUnderlineAnimationForMode(.Do)
+        }
     }
     
     @IBAction func dontButtonPressed(sender:AnyObject) {
-        println("Dont Button Pressed")
-        delegate?.dontButtonDidTouch(sender)
+        
+        if currentButtonSelected == .Do {
+            delegate?.dontButtonDidTouch(sender)
+            performUnderlineAnimationForMode(.Dont)
+        }
     }
+    
 }
