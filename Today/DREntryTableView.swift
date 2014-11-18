c15d195d2eda770c0297ced18f83b2e76f03d3a6
Daniel Rakhamimov <danielrak@me.com>
2014-11-17 21:13:47 -0500

    Add Don't Container View

--- Today/DREntryTableView.swift
+++ Today/DREntryTableView.swift
@@ -15,7 +15,6 @@ class DREntryTableView: UITableView {
         backgroundColor = UIColor.clearColor()
         tableFooterView = UIView(frame: CGRectZero)
         separatorStyle = .None
-        
     }
 }
  
