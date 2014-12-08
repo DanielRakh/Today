00fb8fc4e35b093f26588857850e7031fdd2c1fd
Daniel Rakhamimov <danielrak@me.com>
2014-12-08 17:49:24 -0500

    Button Container Added (no scroll view)

--- Today/DREntryTableViewCell.swift
+++ Today/DREntryTableViewCell.swift
@@ -10,10 +10,26 @@ import UIKit
 
 class DREntryTableViewCell: UITableViewCell {
 
+    /// This is the container for Text View Mode
+    @IBOutlet weak var textContainerView: UIView!
+    /// These are the elements that are subviews of the container
     @IBOutlet weak var alertOnIcon: DRAlertOnIconView!
     @IBOutlet weak var daysLabel: UILabel!
     @IBOutlet weak var lineView: DRGradientView!
     @IBOutlet weak var textView: UITextView!
+    
+    /// This is the container for Button Mode
+    @IBOutlet weak var buttonContainerView: UIView!
+    /// These are the elements that are subviews of the container
+    @IBOutlet weak var trashButton: UIButton!
+    @IBOutlet weak var trashIconView: DRTrashIconView!
+    @IBOutlet weak var checkmarkButton: UIButton!
+    @IBOutlet weak var checkmarkIconView: DRCheckmarkIconView!
+    @IBOutlet weak var alertButton: UIButton!
+    @IBOutlet weak var alertIconView: DRAlertOnIconView!
+    
+    
+    
 
     
     required init(coder aDecoder: NSCoder) {
