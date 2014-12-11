7f1493dddee5b7f8f66cc2759f29154b536f35f2
Daniel Rakhamimov <danielrak@me.com>
2014-12-11 16:56:12 -0500

    AddEntryVC

---
+++ Today/DRAddEntryViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DRAddEntryViewController.swift
+//  Today
+//
+//  Created by Daniel on 12/11/14.
+//  Copyright (c) 2014 Daniel Rakhamimov. All rights reserved.
+//
+
+import UIKit
+
+class DRAddEntryViewController: UIViewController {
+
+}
